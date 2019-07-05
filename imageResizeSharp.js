22835499f0cab793b72acfa35f6c28705f43133c
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-07-05 06:24:27 -0700

    added documentation

--- imageResizeSharp.js
+++ imageResizeSharp.js
@@ -2,18 +2,22 @@ const sharp = require('sharp')
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
 
+// Store Bucket name in environment variable or hardcode it here
 const BUCKET = process.env.BUCKET
 const WIDTH = 100
 const HEIGHT = 100
 
 exports.handler = async (event, context) => {
+  // This gets the first record in the event object (the image data we will use)
   const KEY = event.Records[0].s3.object.key
   const PARTS = KEY.split('/')
+
+  // Here we check to see if the base folder is already set to thumbnails, if it is we return so we do not have a recursive call.
   const BASE_FOLDER = PARTS[0]
   if (BASE_FOLDER === 'thumbnails') return
 
-  const LENGTH = PARTS.length
-  let FILE = PARTS[LENGTH - 1]
+  // This stores the main file name in a variable
+  let FILE = PARTS[PARTS.length - 1]
 
   try {
     const image = await s3.getObject({ Bucket: BUCKET, Key: KEY }).promise()
